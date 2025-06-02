1580205ec8d9f504cf31e6520ff551d5e065d505
alendavies <alendavies10@gmail.com>
2025-06-02 21:04:16 +0000

    fix pipeline

--- spec/dominio/turnero_spec.rb
+++ spec/dominio/turnero_spec.rb
@@ -3,20 +3,18 @@ require_relative '../../dominio/turnero'
 require_relative '../../dominio/medico'
 
 describe Turnero do
-  let(:email)       { 'test@email.com' }
-  let(:turnero)     { Turnero.new(repositorio) }
-  let(:repositorio) { instance_double('RepositorioUsuarios') }
+  let(:email) { 'test@email.com' }
+  let(:repositorio_usuario) { instance_double('RepositorioUsuarios') }
+  let(:repositorio_medico) { instance_double('repositorios_medicos', save: Medico.new('Michael', 'Jordan', 'Traumatologia', 1), all: [Medico.new('Michael', 'Jordan', 'Traumatologia', 1)]) }
+  let(:turnero)     { described_class.new(repositorio_usuario, repositorio_medico) }
 
   it 'deberia crear un medico' do
-    repo_usuarios = instance_double('repositorios_usuarios_dummy')
-    repo_medicos = instance_double('repositorios_medicos', save: Medico.new('Michael', 'Jordan', 'Traumatologia', 1))
-    turnero = described_class.new(repo_usuarios, repo_medicos)
     medico = turnero.crear_medico('Michael', 'Jordan', 'Traumatologia', 1)
     expect(medico.nombre).to eq('Michael')
   end
 
   it 'deberia devolver error si el email ya existe al crear un usuario' do
-    allow(repositorio).to receive(:buscar_por_email).with(email).and_return(Usuario.new(email, 1))
+    allow(repositorio_usuario).to receive(:buscar_por_email).with(email).and_return(Usuario.new(email, 1))
 
     expect { turnero.crear_usuario(email) }.to raise_error(EmailEnUsoException)
   end
@@ -24,16 +22,13 @@ describe Turnero do
   it 'deberia buscar un usuario por email' do
     usuario_mock = Usuario.new(email, 1)
 
-    allow(repositorio).to receive(:buscar_por_email).with(email).and_return(usuario_mock)
+    allow(repositorio_usuario).to receive(:buscar_por_email).with(email).and_return(usuario_mock)
 
     usuario = turnero.buscar_usuario_por_email(email)
     expect(usuario).to eq(usuario_mock)
   end
 
   it 'deberia devolver todos los medicos' do
-    repo_usuarios = instance_double('repositorios_usuarios_dummy')
-    repo_medicos = instance_double('repositorios_medicos', all: [Medico.new('Michael', 'Jordan', 'Traumatologia', 1)])
-    turnero = described_class.new(repo_usuarios, repo_medicos)
     medicos = turnero.medicos
     expect(medicos.first.nombre).to eq('Michael')
   end
