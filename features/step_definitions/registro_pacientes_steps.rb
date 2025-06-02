379238b1094eaba91c31d0fa22e20289c27f991b
alendavies <alendavies10@gmail.com>
2025-06-02 20:25:48 +0000

    Test [US-17]: Agrego steps para el escenario 17.2
    Co-authored-by: Pedro Gallino <pgallino@users.noreply.github.com>

--- features/step_definitions/registro_pacientes_steps.rb
+++ features/step_definitions/registro_pacientes_steps.rb
@@ -12,3 +12,18 @@ Entonces('se registra exitosamente con el mensaje {string}') do |mensaje|
   expect(@response.status).to eq(201)
   expect(@response.body).to include(mensaje)
 end
+
+Dado('el email {string} ya está en uso') do |email|
+  request_body = { email: }.to_json
+  Faraday.post('/usuarios', request_body, { 'Content-Type' => 'application/json' })
+end
+
+Cuando('otro paciente se quiere registrar con el mail {string}') do |mail|
+  request_body = { email: mail }.to_json
+  @response = Faraday.post('/usuarios', request_body, { 'Content-Type' => 'application/json' })
+end
+
+Entonces('se muestra un error con el mensaje {string}') do |mensaje|
+  expect(@response.status).to eq(400)
+  expect(@response.body).to include(mensaje)
+end
