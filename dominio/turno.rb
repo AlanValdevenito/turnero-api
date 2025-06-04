3237f787ec8cb0f19f0360e6cf21efb4c6ef7bdf
Alan <avaldevenito@fi.uba.ar>
2025-06-04 12:41:58 -0300

    feat [US-7]: Turno tiene estado y es pendiente si es nuevo

--- dominio/turno.rb
+++ dominio/turno.rb
@@ -2,10 +2,13 @@ class Turno
   attr_reader :created_on, :updated_on
   attr_accessor :id, :medico, :usuario, :fecha_hora, :estado
 
-  def initialize(medico, usuario, fecha_hora, id = nil)
+  ESTADO_PENDIENTE = 'Pendiente'.freeze
+
+  def initialize(medico, usuario, fecha_hora, estado = ESTADO_PENDIENTE, id = nil)
     @id = id
     @medico = medico
     @usuario = usuario
     @fecha_hora = fecha_hora
+    @estado = estado
   end
 end
