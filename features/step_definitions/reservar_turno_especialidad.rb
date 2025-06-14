4aa249f9da1ec8a0de0303b7e7c63d8b365b730a
Alan <avaldevenito@fi.uba.ar>
2025-06-13 21:41:21 -0300

    test [US-2]: agrego steps definitions para el escenario US-2.1

---
+++ features/step_definitions/reservar_turno_especialidad.rb
@@ -0,0 +1,53 @@
+ESPECIALIDADAS_ALTAS = 2
+
+Dado('que el usuario pide un turno por especialidad') do
+  @response = Faraday.get('/especialidades')
+  expect(@response.status).to eq(200)
+end
+
+Entonces('se muestra un listado de todas las especialidades con su con nombre') do
+  json_response = JSON.parse(@response.body)
+  json_response.each do |especialidad|
+    expect(especialidad).to have_key('nombre')
+  end
+end
+
+Cuando('el usuario selecciona la especialidad {string}') do |especialidad|
+  @response = Faraday.get("/turnos/medicos-disponibles/#{especialidad}")
+end
+
+Entonces('se muestra un listado de {int} médico de la especialidad {string} con su nombre y apellido') do |cantidad_medicos, especialidad|
+  json_response = JSON.parse(@response.body)
+  @matricula = json_response.first['matricula']
+  expect(json_response.size).to eq(cantidad_medicos)
+  json_response.each do |medico|
+    expect(medico['especialidad']).to eq(especialidad)
+    expect(medico).to have_key('nombre')
+    expect(medico).to have_key('apellido')
+  end
+end
+
+Cuando('el usuario selecciona un medico') do
+  @response = Faraday.get("/turnos/#{@matricula}/disponibilidad")
+  expect(@response.status).to eq(200)
+end
+
+Cuando('el usuario selecciona el turno con el medico de matricula {string}') do |matricula|
+  turno_seleccionado = JSON.parse(@response.body).first
+
+  body = { matricula:, fecha: turno_seleccionado['fecha'], hora: turno_seleccionado['hora'], email: @email }
+
+  @response = Faraday.post('/turnos', body.to_json, { 'Content-Type' => 'application/json' })
+  expect(@response.status).to eq(201)
+end
+
+Entonces('se muestra el mensaje de exito {string}') do |mensaje|
+  # Nada
+end
+
+Entonces('se muestra la fecha, hora, medico y especialidad del turno reservado') do
+  turno = JSON.parse(@response.body)
+  expect(turno).to have_key('fecha')
+  expect(turno).to have_key('medico')
+  expect(turno['medico']).to have_key('especialidad')
+end
