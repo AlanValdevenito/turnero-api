5d90d77a5b940b3f18244e8d81cd786a61843276
pgallino <pgallino@fi.uba.ar>
2025-06-15 18:27:47 -0300

    refactor[US-11]: Separo calculadorReputacion de Penalizador

--- dominio/penalizador.rb
+++ dominio/penalizador.rb
@@ -1,28 +1,8 @@
 DURACION_PENALIDAD = 3
 
 class Penalizador
-  def initialize(repositorio_turnos, proveedor_hora)
-    @repositorio_turnos = repositorio_turnos
+  def initialize(proveedor_hora)
     @proveedor_hora = proveedor_hora
-  end
-
-  def calcular_reputacion(usuario)
-    asistidos, cancelados, pendientes, total = contar_turnos(usuario).values
-
-    divisor = total - pendientes
-
-    return 100 if divisor.zero?
-
-    ((asistidos + cancelados).to_f / divisor * 100).round
-  end
-
-  def contar_turnos(usuario)
-    turnos = @repositorio_turnos.buscar_por_usuario(usuario)
-    asistidos = turnos.count { |t| t.estado == ESTADO_ASISTIDO }
-    cancelados = turnos.count { |t| t.estado == ESTADO_CANCELADO }
-    pendientes = turnos.count { |t| t.estado == ESTADO_PENDIENTE }
-    total = turnos.count
-    { asistidos:, cancelados:, pendientes:, total: }
   end
 
   def chequeo_penalizacion_vigente(usuario)
