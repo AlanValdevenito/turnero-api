13833f4c3089a86301ab8471ada7029b1ea92f08
pgallino <pgallino@fi.uba.ar>
2025-06-17 19:06:06 -0300

    test[US-12]: Defino steps 12.1

--- features/auth.feature
+++ features/auth.feature
@@ -3,12 +3,12 @@ Característica: Autenticación por API KEY en la API
 
 Escenario: US-12.1 Acceso denegado sin API KEY
    Cuando envío una request a la API sin incluir una API KEY en el header
-   Entonces la API responde con un mensaje de error de autenticación
+   Entonces la API responde con un mensaje de error de autenticación de key faltante
 
 @wip
 Escenario: US-12.2 Acceso denegado con API KEY inválida
    Cuando envío una request a la API incluyendo una API KEY inválida en el header
-   Entonces la API responde con un mensaje de error de autenticación
+   Entonces la API responde con un mensaje de error de autenticación de key invalida
 
 @wip
 Escenario: US-12.3 Acceso permitido con API KEY válida
--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -2,6 +2,14 @@ Cuando('envío una request a la API sin incluir una API KEY en el header') do
   @response = Faraday.get('/test-api-key')
 end
 
-Entonces('la API responde con un mensaje de error de autenticación') do
+Entonces('la API responde con un mensaje de error de autenticación de key faltante') do
   expect(@response.body).to include('Error de autenticación: falta la API Key')
+end
+
+Cuando('envío una request a la API incluyendo una API KEY inválida en el header') do
+  @response = Faraday.get('/test-api-key', nil, { 'X-API-KEY' => 'INVALIDA' })
+end
+
+Entonces('la API responde con un mensaje de error de autenticación de key invalida') do
+  expect(@response.body).to include('Error de autenticación: API key inválida')
 end
