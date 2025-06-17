a2e3b83b1ba1062d2848b640cfc842949efbe026
alendavies <alendavies10@gmail.com>
2025-06-17 16:54:29 +0000

    feat [US-16]: Solo se tienen en cuenta los turnos pendientes para el limite de turnos

--- dominio/especialidad.rb
+++ dominio/especialidad.rb
@@ -2,6 +2,8 @@ class Especialidad
   attr_reader :created_on, :updated_on
   attr_accessor :id, :nombre, :duracion_de_turnos, :limite_turnos_por_usuario
 
+  ESTADO_PENDIENTE = 'Pendiente'.freeze
+
   def initialize(nombre, duracion_de_turnos, limite_turnos_por_usuario, id = nil)
     @id = id
     @nombre = nombre
@@ -12,10 +14,11 @@ class Especialidad
   def tiene_limite_disponible?(turnos_usuario)
     return true if @limite_turnos_por_usuario.nil?
 
-    turnos_especialidad = turnos_usuario.select do |turno|
-      turno.medico.especialidad.nombre == @nombre
+    turnos_especialidad_pendientes = turnos_usuario.select do |turno|
+      turno.medico.especialidad.nombre == @nombre &&
+        turno.estado == ESTADO_PENDIENTE
     end
 
-    turnos_especialidad.count < @limite_turnos_por_usuario
+    turnos_especialidad_pendientes.count < @limite_turnos_por_usuario
   end
 end
--- spec/dominio/especialidad_spec.rb
+++ spec/dominio/especialidad_spec.rb
@@ -8,8 +8,17 @@ describe Especialidad do
       instance_double('Medico',
                       especialidad: instance_double('Especialidad', nombre: 'Cardiologia'))
     end
-    let(:turno_misma_especialidad) do
-      instance_double('Turno', medico:)
+    let(:turno_pendiente) do
+      instance_double('Turno', medico:, estado: 'Pendiente')
+    end
+    let(:turno_cancelado) do
+      instance_double('Turno', medico:, estado: 'Cancelado')
+    end
+    let(:turno_asistido) do
+      instance_double('Turno', medico:, estado: 'Asistido')
+    end
+    let(:turno_ausente) do
+      instance_double('Turno', medico:, estado: 'Ausente')
     end
 
     it 'devuelve true cuando no hay turnos' do
@@ -17,13 +26,37 @@ describe Especialidad do
     end
 
     it 'devuelve true cuando hay turnos pero menos que el límite' do
-      turnos = [turno_misma_especialidad]
+      turnos = [turno_pendiente]
 
       expect(especialidad.tiene_limite_disponible?(turnos)).to be(true)
     end
 
     it 'devuelve false cuando hay turnos que superan el límite' do
-      turnos = [turno_misma_especialidad, turno_misma_especialidad, turno_misma_especialidad]
+      turnos = [turno_pendiente, turno_pendiente, turno_pendiente]
+
+      expect(especialidad.tiene_limite_disponible?(turnos)).to be(false)
+    end
+
+    it 'devuelve true cuando hay turnos cancelados que no cuentan para el límite' do
+      turnos = [turno_cancelado, turno_cancelado, turno_cancelado, turno_pendiente]
+
+      expect(especialidad.tiene_limite_disponible?(turnos)).to be(true)
+    end
+
+    it 'devuelve true cuando hay turnos asistidos que no cuentan para el límite' do
+      turnos = [turno_asistido, turno_asistido, turno_asistido, turno_pendiente]
+
+      expect(especialidad.tiene_limite_disponible?(turnos)).to be(true)
+    end
+
+    it 'devuelve true cuando hay turnos ausentes que no cuentan para el límite' do
+      turnos = [turno_ausente, turno_ausente, turno_ausente, turno_pendiente]
+
+      expect(especialidad.tiene_limite_disponible?(turnos)).to be(true)
+    end
+
+    it 'devuelve false cuando hay suficientes turnos pendientes para superar el límite' do
+      turnos = [turno_pendiente, turno_cancelado, turno_pendiente, turno_asistido]
 
       expect(especialidad.tiene_limite_disponible?(turnos)).to be(false)
     end
