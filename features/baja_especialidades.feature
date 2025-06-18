1f05bc067e551078b5fee5df8b6dadf6197f797e
Franco Bragantini <francojbragantini@gmail.com>
2025-06-18 15:47:52 -0300

    test [US-21]: agrego step definitions escenario US-21.2

--- features/baja_especialidades.feature
+++ features/baja_especialidades.feature
@@ -5,7 +5,7 @@ Característica: Baja de especialidades
         Dado existe el paciente con email "usuario@prueba.com"
         Y la especialidad "Cardiologia" dada de alta
         Y el médico "Juan Perez" con matrícula "ABC123" de la especialidad "Cardiologia" dado de alta
-        Y el médico "Juan Smith" con matrícula "TRA123" de la especialidad "Cardiologia" dado de alta
+        Y el otro médico "Juan Smith" con matrícula "TRA123" de la especialidad "Cardiologia" dado de alta
 
     Escenario: US-21.1 Dar de baja una especialidad sin medicos
     Dado que la especialidad tiene 0 medicos
@@ -13,8 +13,9 @@ Característica: Baja de especialidades
     Entonces se muestra el mensaje de exitoo "Especialidad eliminada con sus medicos y turnos correspondientes"
     Y no hay turnos correspondientes a la especialidad
     Y la especialidad ya no esta dada de alta
-    @wip
+
     Escenario: US-21.2 Dar de baja una especialidad con medicos
+    Dado que la especialidad tiene dos medicos
     Cuando quiero dar de baja la especialidad
     Entonces se muestra el mensaje de exitoo "Especialidad eliminada con sus medicos y turnos correspondientes"
     Y no hay turnos correspondientes a la especialidad
--- features/step_definitions/baja_especialidades_steps.rb
+++ features/step_definitions/baja_especialidades_steps.rb
@@ -1,6 +1,24 @@
 Dado('que la especialidad tiene {int} medicos') do |_cantidad_medicos|
 end
 
+
+Dado('el otro médico {string} con matrícula {string} de la especialidad {string} dado de alta') do |medico, matricula, especialidad|
+  nombre, apellido = medico.split
+  @matricula2 = matricula
+  request_body = { nombre:, apellido:, matricula:, especialidad: }.to_json
+  @response = api_post('/medicos', request_body)
+  expect(@response.status).to eq(200)
+end
+
+Dado('que la especialidad tiene dos medicos') do
+  medico = RepositorioMedicos.new.buscar_por_matricula(@matricula)
+  medico2 = RepositorioMedicos.new.buscar_por_matricula(@matricula2)
+  usuario = RepositorioUsuarios.new.buscar_por_email(@email)
+  fecha = Date.parse('2025-06-18')
+  RepositorioTurnos.new.save(Turno.new(medico, usuario, fecha))
+  RepositorioTurnos.new.save(Turno.new(medico2, usuario, fecha))
+end
+
 Cuando('quiero dar de baja la especialidad') do
   @response = api_delete("/especialidades/#{@especialidad}")
   expect(@response.status).to eq(200)
@@ -13,4 +31,11 @@ end
 Entonces('la especialidad ya no esta dada de alta') do
   response = api_get("/especialidades/#{@matricula}")
   expect(response.status).to eq(404)
+end
+
+Entonces('los medicos de la especialidad ya no estan dados de alta') do
+  medico = RepositorioMedicos.new.buscar_por_matricula(@matricula)
+  expect(medico).to be_nil
+  medico2 = RepositorioMedicos.new.buscar_por_matricula(@Matricula2)
+  expect(medico2).to be_nil
 end
